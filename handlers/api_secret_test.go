9f199517dad573b1abf3970443a919d8b1193279
nqd <dinh.nguyen@veriksystems.com>
2019-04-26 22:08:00 +0700

    rename package name

---
+++ handlers/api_secret_test.go
@@ -0,0 +1,6 @@
+package handlers
+
+import "testing"
+
+func TestInit(t *testing.T) {
+}
--- main.go
+++ main.go
@@ -13,20 +13,13 @@ import (
 	"log"
 	"net/http"
 
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//    sw "github.com/myname/myrepo/go"
-	//
-	sw "./go"
+	"github.com/nqd/golang-w2/handlers"
 )
 
 func main() {
 	log.Printf("Server started")
 
-	router := sw.NewRouter()
+	router := handlers.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
